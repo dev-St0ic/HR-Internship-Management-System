994dfdda0091b2a039e003ec5b9061ada9963f29
AaronAngat <aaronangat30@gmail.com>
2026-03-23 17:42:40 +0800

    Frontend folder structure

---
+++ frontend-hrms/src/routes/AppRouter.jsx
@@ -0,0 +1,36 @@
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Temporary placeholders muna
+const InternDashboard = () => (
+    <>Intern dashboard page</>
+);
+
+const SupervisorDashboard = () => (
+    <>Supervisor dashboardpage</>
+);
+
+const HRStaffDashboard = () => (
+    <>HR staff dashboard page</>
+);
+
+const HRAdminDashboard = () => (
+    <>HR admin dashboard page</>
+);
+
+
+
+export const AppRouter = () => {
+    return (
+    <BrowserRouter>
+        <Routes>
+
+            <Route path="/intern/*" element={<InternDashboard />} />
+            <Route path="/supervisor/*" element={<SupervisorDashboard />} />
+            <Route path="/hr-staff/*" element={<HRStaffDashboard />} />
+            <Route path="/hr-admin/*" element={<HRAdminDashboard />} />
+
+        </Routes>
+    </BrowserRouter>
+    );
+};
