c4f440f170b1ce86300e141691cd24daad4b6606
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2026-04-06 14:41:25 +0800

    added the navigation configuration for all the users

--- frontend-hrms/src/common/config/navigation.jsx
+++ frontend-hrms/src/common/config/navigation.jsx
@@ -6,12 +6,127 @@ import {
   FileText,
   NotebookPen,
   Settings,
+  Users,
+  BriefcaseBusiness,
+  GitCompare,
+  CalendarCheck2,
+  FolderGit2,
+  ScrollText,
 } from "lucide-react";
 
 export const navigation = {
-  hradmin: [],
-  hrstaff: [],
-  supervisor: [],
+  hradmin: [
+    {
+      to: "/hr-admin",
+      label: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+      end: true,
+    },
+    {
+      to: "/hr-admin/recruitment",
+      label: "Recruitment",
+      icon: <Users size={18} />,
+    },
+    {
+      to: "/hr-admin/internmanagement",
+      label: "Intern Management",
+      icon: <GitCompare size={18} />,
+    },
+    {
+      to: "/hr-admin/staffmanagement",
+      label: "Staff Management",
+      icon: <CalendarCheck2 size={18} />,
+    },
+    {
+      to: "/hr-admin/documnetvault",
+      label: "Document Vault",
+      icon: <FileText size={18} />,
+    },
+    {
+      to: "/hr-admin/reportsandanalytics",
+      label: "Reports & Analytics",
+      icon: <FolderGit2 size={18} />,
+    },
+    {
+      to: "/hr-admin/systemlogs",
+      label: "System Logs",
+      icon: <ScrollText size={18} />,
+    },
+    {
+      to: "/hr-admin/settings",
+      label: "Settings",
+      icon: <Settings size={18} />,
+    },
+  ],
+
+  hrstaff: [
+    {
+      to: "/hr-staff",
+      label: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+      end: true,
+    },
+    {
+      to: "/hr-staff/recruitment",
+      label: "Recruitment",
+      icon: <Users size={18} />,
+    },
+    {
+      to: "/hr-staff/operations",
+      label: "Operations",
+      icon: <GitCompare size={18} />,
+    },
+    {
+      to: "/hr-staff/staffmanagment",
+      label: "Staff Management",
+      icon: <CalendarCheck2 size={18} />,
+    },
+    {
+      to: "/hr-staff/documentvault",
+      label: "Document Vault",
+      icon: <FileText size={18} />,
+    },
+    {
+      to: "/hr-staff/settings",
+      label: "Settings",
+      icon: <Settings size={18} />,
+    },
+  ],
+
+  supervisor: [
+    {
+      to: "/supervisor",
+      label: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+      end: true,
+    },
+    {
+      to: "/supervisor/myinterns",
+      label: "My Interns",
+      icon: <Users size={18} />,
+    },
+    {
+      to: "/supervisor/attendance",
+      label: "Attendance",
+      icon: <CalendarCheck size={18} />,
+    },
+    {
+      to: "/supervisor/tasks",
+      label: "Tasks",
+      icon: <ClipboardList size={18} />,
+    },
+    {
+      to: "/supervisor/evaluation",
+      label: "Evaluation",
+      icon: <BriefcaseBusiness size={18} />,
+    },
+    {
+      to: "/supervisor/settings",
+      label: "Settings",
+      icon: <Settings size={18} />,
+    },
+  ],
+
   intern: [
     {
       to: "/intern",
