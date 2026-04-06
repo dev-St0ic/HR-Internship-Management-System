1789d673dad34bfb88bcf9fcdbbfb1c1d77fbf39
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2026-04-06 12:46:31 +0800

    checked the sidebar

--- frontend-hrms/src/portals/supervisor/components/Sidebar.jsx
+++ frontend-hrms/src/portals/supervisor/components/Sidebar.jsx
@@ -1,27 +1,39 @@
-import { NavLink } from 'react-router-dom';
-import { LayoutDashboard, Users, CalendarCheck, ClipboardList, BriefcaseBusiness, Settings  } from 'lucide-react';
-import SidebarProfile from './SidebarProfile';
-
+import { NavLink } from "react-router-dom";
+import {
+  LayoutDashboard,
+  Users,
+  CalendarCheck,
+  ClipboardList,
+  BriefcaseBusiness,
+  Settings,
+} from "lucide-react";
+import SidebarProfile from "./SidebarProfile";
 
 const baseClass = "block p-3 mt-1 rounded-r-lg transition-colors duration-300";
-const activeClass = "border-l-4 border-[#7C3EFF] font-medium text-[#7C3EFF] bg-gray-300/70";
+const activeClass =
+  "border-l-4 border-[#7C3EFF] font-medium text-[#7C3EFF] bg-gray-300/70";
 const inactiveClass = "hover:bg-gray-300/70";
 
 const getNavClass = ({ isActive }) =>
   `${baseClass} ${isActive ? activeClass : inactiveClass}`;
 
-
 export default function Sidebar() {
   return (
-    <nav className="p-7 w-[300px] h-[calc(100vh-2rem)] sticky top-5 bottom-5 m-5 rounded-2xl text-gray-700 shadow-lg" style={{ background: '#fafafb' }}>
+    <nav
+      className="p-7 w-[300px] h-[calc(100vh-2rem)] sticky top-5 bottom-5 m-5 rounded-2xl text-gray-700 shadow-lg"
+      style={{ background: "#fafafb" }}
+    >
       <div className="logo-company flex items-center text-lg font-medium text-gray-700">
-        <img src="image.png" alt="" className="size-15 rounded-full p-0 m-0" style={{background: '#7C3EFF'}} /> 
+        <img
+          src="image.png"
+          alt=""
+          className="size-15 rounded-full p-0 m-0"
+          style={{ background: "#7C3EFF" }}
+        />
         <h1 className="ml-2">SUPERVISOR</h1>
       </div>
       <div className="nav-tab mt-5">
-
         <ul className="nav">
-
           <li>
             <NavLink to="/s" end className={getNavClass}>
               <div className="flex justify-start items-center gap-2">
@@ -51,11 +63,11 @@ export default function Sidebar() {
               <div className="flex justify-start items-center gap-2">
                 <ClipboardList /> Tasks
               </div>
-            </NavLink>  
+            </NavLink>
           </li>
-          
+
           <li>
-            <NavLink to="/s/evaluations" className={getNavClass}> 
+            <NavLink to="/s/evaluations" className={getNavClass}>
               <div className="flex justify-start items-center gap-2">
                 <BriefcaseBusiness /> Evaluations
               </div>
@@ -68,11 +80,9 @@ export default function Sidebar() {
               </div>
             </NavLink>
           </li>
-
         </ul>
         {/* sidebar footer */}
-          <SidebarProfile />
-        
+        <SidebarProfile />
       </div>
     </nav>
   );
