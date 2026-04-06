3995d2ea38180ab44855b98480252f374fdbe4ba
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2026-04-06 17:11:28 +0800

    moved the reusable sidebar at the components file under layout

---
+++ frontend-hrms/src/common/components/layout/Sidebar.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+
+const baseClass =
+  "block w-full p-3 mt-1 rounded-r-lg transition-colors duration-300 flex items-center gap-3";
+
+const activeClass =
+  "bg-gray-200 border-l-4 border-[#7C3EFF] font-medium text-[#7C3EFF] bg-gray-300/70";
+const inactiveClass = "hover:bg-gray-300/70";
+
+const getNavClass = ({ isActive }) =>
+  `${baseClass} ${isActive ? activeClass : inactiveClass}`;
+
+const Sidebar = ({ links }) => {
+  return (
+    <nav className="fixed top-0 left-0 h-screen w-[300px] p-7 bg-[#fafafb] overflow-y-auto ">
+      {/* LOGO */}
+      <div className="mb-6">
+        <img
+          src="/image.png"
+          alt="logo"
+          className="w-12 h-12 rounded-full bg-gray-300"
+        />
+      </div>
+
+      {/* NAVIGATION */}
+      <ul>
+        {links.map((link) => {
+          const Icon = link.icon;
+
+          return (
+            <li key={link.to}>
+              <NavLink to={link.to} end={link.end} className={getNavClass}>
+                {Icon && <Icon size={18} />}
+                <span>{link.label}</span>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidebar;
