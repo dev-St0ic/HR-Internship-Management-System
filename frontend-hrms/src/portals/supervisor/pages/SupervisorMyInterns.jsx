9caedd7f25aa644b956f04d15cf47822f5f5a120
Jonathan Dionson <dionsonjonathan8@gmail.com>
2026-04-07 15:55:22 +0800

    create settings page

--- frontend-hrms/src/portals/supervisor/pages/SupervisorMyInterns.jsx
+++ frontend-hrms/src/portals/supervisor/pages/SupervisorMyInterns.jsx
@@ -2,31 +2,27 @@ import { Search, SlidersHorizontal } from "lucide-react";
 
 export default function SupervisorMyInterns() {
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+    <div className="p-6 min-h-screen">
 
-      {/* TOP BAR */}
-      <div className="flex items-center gap-3 mb-4">
+      <div className="flex items-center justify-between gap-3 mb-4">
         
-        {/* SEARCH INPUT */}
-        <div className="flex items-center w-full max-w-md bg-purple border border-purple-600 rounded-lg px-3 py-2">
-          <Search className="w-4 h-4 text-purple-600  mr-2" />
+        <div className="flex items-center w-full max-w-md bg-purple border border-[#A2A1A81A] rounded-lg px-3 py-2">
+          <Search className="w-4 h-4 text-gray-600  mr-2" />
           <input
             type="text"
             placeholder="Search interns..."
-            className="w-full outline-none text-sm font-medium text-purple-600 placeholder-purple-600"
+            className="w-full outline-none text-sm font-medium text-[#16151C33]"
           />
         </div>
 
-        {/* FILTER BUTTON */}
-        <button className="flex items-center gap-2 px-4 py-2 bg-purple border border-purple-600 rounded-lg text-sm font-medium text-purple-600 hover:bg-purple-50">
-          <SlidersHorizontal className="w-4 h-4" />
-          Filter
-        </button>
+        <select className="bg-gray-500/20 p-2 rounded-md border-0">
+                <option>Filter</option>
+                <option>Filter</option>
+              </select>
       </div>
 
-      {/* TABLE CONTAINER */}
       <div className="bg-white rounded-xl border border-gray-200 overflow-hidden">
-        <table className="w-full text-sm text-gray-600">
+        <table className="table-auto w-full text-md text-gray-600">
           
           <thead className="text-gray-400 text-xs border-b">
             <tr>
@@ -41,73 +37,91 @@ export default function SupervisorMyInterns() {
 
           <tbody className="divide-y divide-gray-100">
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Juan Dela Cruz</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">IT (All Dept)</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-                <span className="bg-purple-500 text-white-1000 text-xs px-3 font- py-1 rounded-md">
+                <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
             </tr>
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Ana Reyes</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">Marketing</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-                <span className="bg-purple-500 text-white-1000 text-xs px-3 py-1 rounded-md">
+                <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
             </tr>
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Carlos Garcia</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">Multimedia</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-               <span className="bg-purple-500 text-white-100 text-xs px-3 py-1 rounded-md">
+               <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
             </tr>
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Maria Lopez</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">Special Project</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-                <span className="bg-purple-500 text-white-1000 text-xs px-3 py-1 rounded-md">
+                <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
             </tr>
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Pedro Santos</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">Finance</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-                <span className="bg-purple-500 text-white-1000 text-xs px-3 py-1 rounded-md">
+                <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
             </tr>
             <tr className="hover:bg-gray-50 font-medium">
-              <td className="px-6 py-4">Cara Lim</td>
+              <td className="px-6 py-4 flex items-center">
+                <img src="../../image.png" className="size-10 bg-[#7152F3] rounded-full mr-2" />
+                Juan Dela Cruz
+                </td>
               <td className="px-6 py-4">Web Development</td>
               <td className="px-6 py-4">University</td>
               <td className="px-6 py-4">Program</td>
               <td className="px-6 py-4">Date</td>
               <td className="px-6 py-4">
-                <span className="bg-purple-500 text-white-1000 text-xs px-3 py-1 rounded-md">
+                <span className="bg-[#7152F31A] text-[#7152F3] text-xs px-3 font- py-1 rounded-md">
                   Active
                 </span>
               </td>
--- frontend-hrms/src/portals/supervisor/pages/SupervisorSettings.jsx
+++ frontend-hrms/src/portals/supervisor/pages/SupervisorSettings.jsx
@@ -1,8 +1,204 @@
+import { useState } from "react";
+import { Settings, User } from "lucide-react";
+
 export default function SupervisorSettings() {
+  const [activeTab, setActiveTab] = useState("general");
+
   return (
-    <>
-      <h1>Supervisor Settings</h1>
-      <p>This is the Supervisor Settings page.</p>
-    </>
+    <div className="border border-gray-500/20 rounded-lg p-4">
+
+      <div className="border-b border-gray-300">
+        <ul className="flex text-sm font-medium text-gray-500">
+
+          <li className="mr-2">
+            <button
+              onClick={() => setActiveTab("general")}
+              className={`inline-flex items-center p-4 border-b-2 rounded-t-lg ${activeTab === "general"
+                ? "text-violet-600 border-violet-600"
+                : "border-transparent hover:text-violet-600 hover:border-violet-600"
+                }`}
+            >
+              <Settings className="mr-2" size={18} />
+              General Preferences
+            </button>
+          </li>
+
+          <li className="mr-2">
+            <button
+              onClick={() => setActiveTab("profile")}
+              className={`inline-flex items-center p-4 border-b-2 rounded-t-lg ${activeTab === "profile"
+                ? "text-violet-600 border-violet-600"
+                : "border-transparent hover:text-violet-600 hover:border-violet-600"
+                }`}
+            >
+              <User className="mr-2" size={18} />
+              Profile
+            </button>
+          </li>
+
+        </ul>
+      </div>
+
+      <div className="border border-gray-500/20 rounded-lg p-5 mt-5">
+
+        {activeTab === "general" && (
+          <div className="divide-y divide-gray-300">
+
+            <div className='flex justify-between items-center py-5'>
+              <div>
+                <h1 className="font-semibold">Appearance</h1>
+                <span className="text-gray-500">Customize how your theme looks</span>
+              </div>
+              <select className="bg-[#A2A1A81A] p-2 rounded-lg border-0">
+                <option>Light</option>
+                <option>Dark</option>
+              </select>
+            </div>
+
+            <div className='flex justify-between items-center py-5'>
+              <div>
+                <h1 className="font-semibold">Date and Time Format</h1>
+                <span className="text-gray-500">Select your localization</span>
+              </div>
+              <select className="bg-[#A2A1A81A] p-2 rounded-lg border-0">
+                <option>YYYY-MM-DD | HH:MM:SS</option>
+                <option>DD-MM-YY | SS:MM:HH</option>
+              </select>
+            </div>
+
+            <div className='flex justify-between items-center border-b border-gray-300 py-5'>
+              <div>
+                <h1 className="font-semibold">Two-Factor Authentication</h1>
+                <span className="text-gray-500">Keep your account secure with 2FA via mail</span>
+              </div>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" className="sr-only peer" />
+                <div className="w-16 h-8 bg-slate-300 rounded-full peer-checked:bg-[#1BCB3E] transition-colors"></div>
+                <span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition-transform peer-checked:translate-x-8"></span>
+              </label>
+            </div>
+
+            <div className='flex justify-between items-center border-b border-gray-300 py-5'>
+              <div>
+                <h1 className="font-semibold">Mobile Push Notifications</h1>
+                <span className="text-gray-500">Receive push notification</span>
+              </div>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" className="sr-only peer" />
+                <div className="w-16 h-8 bg-slate-300 rounded-full peer-checked:bg-[#1BCB3E] transition-colors"></div>
+                <span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition-transform peer-checked:translate-x-8"></span>
+              </label>
+            </div>
+
+            <div className='flex justify-between items-center border-b border-gray-300 py-5'>
+              <div>
+                <h1 className="font-semibold">Desktop Notification</h1>
+                <span className="text-gray-500">Receive push notifications in desktop</span>
+              </div>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" className="sr-only peer" />
+                <div className="w-16 h-8 bg-slate-300 rounded-full peer-checked:bg-[#1BCB3E] transition-colors"></div>
+                <span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition-transform peer-checked:translate-x-8"></span>
+              </label>
+            </div>
+
+            <div className='flex justify-between items-center border-b border-gray-300 py-5'>
+              <div>
+                <h1 className="font-semibold">Email Notifications</h1>
+                <span className="text-gray-500">Receive email notification</span>
+              </div>
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" className="sr-only peer" />
+                <div className="w-16 h-8 bg-slate-300 rounded-full peer-checked:bg-[#1BCB3E] transition-colors"></div>
+                <span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition-transform peer-checked:translate-x-8"></span>
+              </label>
+            </div>
+
+          </div>
+        )}
+
+        {activeTab === "profile" && (
+          <div className="divide-y divide-gray-300">
+            <div className='items-center py-5'>
+              <h2 class="text-lg font-semibold text-gray-800 mb-4">Account Settings</h2>
+
+              <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+                <div>
+                  <label class="block text-sm text-gray-500 mb-1">User Name</label>
+                  <input type="text" placeholder="User Name"
+                    class="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2" />
+                </div>
+                <div>
+                  <label class="block text-sm text-gray-500 mb-1">Email Address</label>
+                  <input type="email" value="Supervisor@email.com"
+                    class="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm focus:outline-none" />
+                </div>
+                <div>
+                  <label class="block text-sm text-gray-500 mb-1">Password</label>
+                  <input type="password" value="***************"
+                    class="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm focus:outline-none" />
+                </div>
+                <div>
+                  <label class="block text-sm text-gray-500 mb-1">New Password</label>
+                  <input type="password" placeholder="New Password"
+                    class="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2" />
+                </div>
+              </div>
+            </div>
+
+            <div className="border-gray-30 py-5">
+              <h2 className="text-lg font-semibold text-gray-800 mb-4">
+                Personal Information
+              </h2>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                <div>
+                  <label className="block text-sm text-gray-500 mb-1">First Name</label>
+                  <input type="text" className="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm" />
+                </div>
+
+                <div>
+                  <label className="block text-sm text-gray-500 mb-1">Last Name</label>
+                  <input type="text" className="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm" />
+                </div>
+
+                <div>
+                  <label className="block text-sm text-gray-500 mb-1">Mobile Number</label>
+                  <input type="text" className="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm" />
+                </div>
+
+                <div>
+                  <label className="block text-sm text-gray-500 mb-1">Gender</label>
+                  <select className="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm">
+                    <option>Gender</option>
+                    <option>Male</option>
+                    <option>Female</option>
+                  </select>
+                </div>
+
+                <div className="md:col-span-2">
+                  <label className="block text-sm text-gray-500 mb-1">Date of Birth</label>
+                  <input type="date" className="w-full border border-gray-200 rounded-lg px-3 py-2 text-sm" />
+                </div>
+
+              </div>
+
+              <div className="flex justify-end gap-3 mt-6">
+                <button className="px-4 py-2 border border-gray-300 rounded-lg text-sm text-gray-600">
+                  Cancel
+                </button>
+                <button className="px-4 py-2 text-white rounded-lg bg-[#7C3EFF]">
+                  Save Changes
+                </button>
+              </div>
+
+            </div>
+          </div>
+
+        )}
+
+      </div>
+    </div>
   );
 }
