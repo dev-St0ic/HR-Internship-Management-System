7dd09e3d457b426e3e7ca4f637b94f91cbce0138
AaronAngat <aaronangat30@gmail.com>
2026-04-08 17:45:57 +0800

    created login page (otp and forgot pass not finished yet)

--- frontend-hrms/src/portals/interns/pages/LoginPage.jsx
+++ frontend-hrms/src/portals/interns/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import LoginForm from "../components/auth/LoginForm";
 import ForgotPasswordForm from "../components/auth/ForgotPasswordForm";
 import OtpForm from "../components/auth/OtpForm";
+import LoginIllustration from "../components/auth/LoginIllustration";
 
 
 
@@ -12,13 +13,12 @@ export default function LoginPage() {
         <div className="h-screen w-full bg-white font-lexend flex items-center justify-center p-[10px] md:p-3">
             <div className="flex w-full h-full max-w-[5000px]">
                 
-                <div className="hidden md:block w-4/6 bg-[#efefef] rounded-[2rem]"></div>
+                <div className="hidden md:flex md:w-1/2 lg:w-[55%] bg-[#F8F8FC] rounded-[2rem] items-center justify-center">
+                    <LoginIllustration />
+                </div>
                     <div className="w-full md:w-2/5 flex flex-col justify-center px-6 md:px-8 lg:px-16 xl:px-24 overflow-y-auto">
                     
-                    <div className="flex items-center gap-3 mb-12">
-                        <div className="w-10 h-10 bg-[#d9d9d9] rounded-full"></div>
-                        <h1 className="text-2xl font-bold text-gray-900 tracking-wide">HRMS</h1>
-                    </div>
+                    
 
                     {currentView === "login" && (
                         <LoginForm onForgotPassword={() => setCurrentView("forgot")} />
