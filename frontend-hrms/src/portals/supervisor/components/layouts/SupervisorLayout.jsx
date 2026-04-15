4c59f12311fedb818af68cdcb22d670a0180ba75
Rain-Ryn <marklasquity@gmail.com>
2026-04-15 13:19:33 +0800

    add sidebar profile name and role

--- frontend-hrms/src/portals/supervisor/components/layouts/SupervisorLayout.jsx
+++ frontend-hrms/src/portals/supervisor/components/layouts/SupervisorLayout.jsx
@@ -4,15 +4,18 @@ import { navigation } from "../../../../common/config/navigation";
 
 export default function SupervisorLayout() {
   const userRole = "supervisor"; 
-  
   const navLinks = navigation[userRole] || [];
 
   return (
     <div className="flex">
-      {/* Sidebar should always be visible */}
-      <Sidebar links={navLinks} />
+      
+      {/* ADD THE ROLE PROP HERE */}
+      <Sidebar links={navLinks} 
+      
+      role="Supervisor" userName="Cale Andren"/>  {/* Temp Sidebar Profile Name & Role */}
+      
+      
 
-      {/* Main content area */}
       <div className="ml-60 p-6 flex-1">
         <Outlet />
       </div>
