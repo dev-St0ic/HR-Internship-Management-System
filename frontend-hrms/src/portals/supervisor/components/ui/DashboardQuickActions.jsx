c66f241691b24e6a213d8923a66a00e836dc9be8
Jonathan Dionson <dionsonjonathan8@gmail.com>
2026-04-27 13:36:48 +0800

    add: link routes for supervisor dashboard 'Quick Actions'

--- frontend-hrms/src/portals/supervisor/components/ui/DashboardQuickActions.jsx
+++ frontend-hrms/src/portals/supervisor/components/ui/DashboardQuickActions.jsx
@@ -1,4 +1,5 @@
 import { NotebookText, BookCheck, LibraryBig } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 export default function DashboardQuickActions() {
   return (
@@ -6,17 +7,17 @@ export default function DashboardQuickActions() {
           <h1 className="font-medium text-lg">Quick Actions</h1>
           <div className="button ms-3 flex flex-wrap gap-3 mt-2 justify-between">
 
-            <button className="bg-gray-500/10 text-[#7C3EFF] py-2 px-4 rounded hover:bg-gray-500/20 cursor-pointer transition-colors text-md flex items-center">
+            <Link to="/supervisor/attendance" className="bg-gray-500/10 text-[#7C3EFF] py-2 px-4 rounded hover:bg-gray-500/20 cursor-pointer transition-colors text-md flex items-center">
               <NotebookText className="w-5 me-1" /> Review DTRs
-            </button>
+            </Link>
 
-            <button className="bg-[#7C3EFF] text-white py-2 px-4 rounded hover:bg-[#6a30d5] cursor-pointer transition-colors text-md flex items-center">
+            <Link to="/supervisor/tasks" className="bg-[#7C3EFF] text-white py-2 px-4 rounded hover:bg-[#6a30d5] cursor-pointer transition-colors text-md flex items-center">
               <BookCheck className="w-5 me-1" /> Assign New Tasks
-            </button>
-
-            <button className="bg-[#7C3EFF] text-white py-2 px-4 rounded hover:bg-[#6a30d5] cursor-pointer transition-colors text-md flex items-center">
+            </Link>
+{/* oks na */}
+            <Link to="/supervisor/evaluations" className="bg-[#7C3EFF] text-white py-2 px-4 rounded hover:bg-[#6a30d5] cursor-pointer transition-colors text-md flex items-center">
               <LibraryBig className="w-5 me-1" /> Start Evaluation
-            </button>
+            </Link>
 
           </div>
         </div>
