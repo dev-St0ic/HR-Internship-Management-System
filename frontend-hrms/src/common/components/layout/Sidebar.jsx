b28ef9d7832b2d608114af4e33a7deb4d98fd6db
W0LVEZ <133645225+W0LVEZ@users.noreply.github.com>
2026-04-28 08:08:06 +0800

    Made the sidebar collapsable into the icons only

--- frontend-hrms/src/common/components/layout/Sidebar.jsx
+++ frontend-hrms/src/common/components/layout/Sidebar.jsx
@@ -1,53 +1,78 @@
 import { NavLink } from "react-router-dom";
 import SidebarProfile from "./SidebarProfile";
 
-const baseClass =
-  "block w-full p-3 mt-1 rounded-r-lg transition-colors duration-300 flex items-center gap-3";
-
 const activeClass =
   "bg-gray-200 border-l-4 border-[#7C3EFF] font-medium text-[#7C3EFF] bg-gray-300/70";
 const inactiveClass = "hover:bg-gray-300/70";
 
-const getNavClass = ({ isActive }) =>
-  `${baseClass} ${isActive ? activeClass : inactiveClass}`;
+const Sidebar = ({
+  links = [],
+  userName,
+  role,
+  isCollapsed,
+  setIsCollapsed,
+}) => {
+  const baseClass = `w-full p-3 mt-1 rounded-r-lg transition-colors duration-300 flex items-center gap-3 ${
+    isCollapsed ? "justify-center" : "gap-3"
+  }`;
 
-const Sidebar = ({ links = [], userName, role }) => {
+  const getNavClass = ({ isActive }) =>
+    `${baseClass} ${isActive ? activeClass : inactiveClass}`;
+
   return (
-    <nav className="fixed top-0 left-0 h-screen w-60 p-7 bg-[#FFFFFF] overflow-y-auto rounded-2xl text-gray-700 shadow-lg flex flex-col justify-between">
+    <nav
+      className={`fixed top-0 left-0 h-screen bg-white overflow-y-auto rounded-2xl text-gray-700 shadow-lg flex flex-col justify-between transition-all duration-300
+    ${isCollapsed ? "w-24 p-4" : "w-60 p-7"}`}
+    >
       <div>
         {/* LOGO */}
-        <div className="flex items-center text-4xl font-medium mb-6">
+        <button
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          className={`flex items-center text-4xl font-medium mb-6 w-full ${
+            isCollapsed ? "justify-center" : ""
+          }`}
+        >
           <img
             src="/image.png"
             alt="logo"
             className="w-12 h-12 rounded-full bg-violet-500"
           />
-          <h1 className="ml-2">HRIMS</h1>
-        </div>
+          {!isCollapsed && <h1 className="ml-2">HRIMS</h1>}
+        </button>
 
         {/* NAVIGATION */}
         <ul>
-          {links.length > 0 ? (
-            links.map((link) => {
-              const Icon = link.icon;
+          {links.length > 0
+            ? links.map((link) => {
+                const Icon = link.icon;
 
-              return (
-                <li key={link.to}>
-                  <NavLink to={link.to} end={link.end} className={getNavClass}>
-                    {Icon && <Icon size={18} />}
-                    <span className="truncate">{link.label}</span>
-                  </NavLink>
-                </li>
-              );
-            })
-          ) : (
-            <p className="text-sm text-gray-400">No navigation available</p>
-          )}
+                return (
+                  <li key={link.to}>
+                    <NavLink
+                      to={link.to}
+                      end={link.end}
+                      className={getNavClass}
+                    >
+                      {Icon && <Icon size={18} />}
+                      {!isCollapsed && (
+                        <span className="truncate">{link.label}</span>
+                      )}
+                    </NavLink>
+                  </li>
+                );
+              })
+            : !isCollapsed && (
+                <p className="text-sm text-gray-400">No navigation available</p>
+              )}
         </ul>
       </div>
 
       {/* PROFILE ROLE */}
-      <SidebarProfile userName={userName} role={role} />
+      <SidebarProfile
+        userName={userName}
+        role={role}
+        isCollapsed={isCollapsed}
+      />
     </nav>
   );
 };
